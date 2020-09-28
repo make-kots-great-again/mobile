d72f3ded02d01825edab3ed61077443c71c22f36
momo007Dev <m.valentin@students.ephec.be>
2020-09-28 18:54:30 +0200

    started project

---
+++ makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/MainActivity.java
@@ -0,0 +1,14 @@
+package com.morgan.make_kots_great_again;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
