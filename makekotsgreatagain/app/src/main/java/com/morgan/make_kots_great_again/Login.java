097418dd98ad55976a215b3b60173e69e1239639
momo007Dev <m.valentin@students.ephec.be>
2020-10-03 16:14:08 +0200

    ajout des 3 pages : Login, page1 page2

---
+++ makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/Login.java
@@ -0,0 +1,14 @@
+package com.morgan.make_kots_great_again;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login);
+    }
+}
