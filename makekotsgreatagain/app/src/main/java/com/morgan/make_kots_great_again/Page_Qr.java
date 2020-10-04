043d81f845e41a3db254b31d9a112aaabe8c6ee8
AutomeEdwin <automeedwin@gmail.com>
2020-10-04 14:49:26 +0200

    Added QR Layout

---
+++ makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/Page_Qr.java
@@ -0,0 +1,17 @@
+package com.morgan.make_kots_great_again;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Page_Qr extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.page_qr);
+
+        //Mon code...
+
+    }
+}
