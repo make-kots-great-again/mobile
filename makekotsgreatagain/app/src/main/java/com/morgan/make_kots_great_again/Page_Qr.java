869b61a11f40c5e84a0ee5ee448d8d61613db7f9
AutomeEdwin <automeedwin@gmail.com>
2020-10-08 10:48:05 +0200

    Removed previous code and added intent to use camera application

--- makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/Page_Qr.java
+++ makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/Page_Qr.java
@@ -1,17 +1,29 @@
 package com.morgan.make_kots_great_again;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class Page_Qr extends AppCompatActivity {
 
+    private Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+    private int REQUEST_ID_IMAGE_CAPTURE = 100;
+
+    // METHODES DE CYCLE DE VIE ANDROID
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(final Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page_qr);
 
-        //Mon code...
+        // Lauching camera application
+        this.startActivityForResult(intent, REQUEST_ID_IMAGE_CAPTURE);
 
+        //Stop activity when leave camera application
+        finish();
     }
+
 }
