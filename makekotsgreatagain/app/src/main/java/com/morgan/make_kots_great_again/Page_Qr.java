d934ded0480b22c04fd9758c58ff9c7601ce1551
AutomeEdwin <automeedwin@gmail.com>
2020-10-10 21:28:16 +0200

    Now able to read QR codes

--- makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/Page_Qr.java
+++ makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/Page_Qr.java
@@ -1,29 +1,104 @@
 package com.morgan.make_kots_great_again;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.util.SparseArray;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.barcode.Barcode;
+import com.google.android.gms.vision.barcode.BarcodeDetector;
+
+import java.io.IOException;
 
 public class Page_Qr extends AppCompatActivity {
 
-    private Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-    private int REQUEST_ID_IMAGE_CAPTURE = 100;
+    //VARIABLES
+    private SurfaceView surfaceView;
+    private CameraSource cameraSource;
+    private TextView textView;
+    private BarcodeDetector barcodeDetector;
 
     // METHODES DE CYCLE DE VIE ANDROID
     @Override
-    protected void onCreate(final Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.page_qr);
+        surfaceView = findViewById(R.id.cameraPreview);
+        textView = findViewById(R.id.textView);
 
-        // Lauching camera application
-        this.startActivityForResult(intent, REQUEST_ID_IMAGE_CAPTURE);
+        barcodeDetector = new BarcodeDetector.Builder(this)
+                .setBarcodeFormats(Barcode.QR_CODE)
+                .build();
 
-        //Stop activity when leave camera application
-        finish();
+        //Setting up the camera
+        cameraSource = new CameraSource.Builder(getApplicationContext(), barcodeDetector)
+                .setRequestedPreviewSize(640, 480)
+                .build();
+
+        surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(@NonNull SurfaceHolder holder) {
+                try
+                {
+                    if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                        launch_Login_Activity();
+                        return;
+                    }
+
+                    cameraSource.start(holder);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        barcodeDetector.setProcessor(new Detector.Processor<Barcode>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<Barcode> detections) {
+                final SparseArray<Barcode> qrCodes = detections.getDetectedItems();
+
+                if(qrCodes.size() != 0)
+                {
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            textView.setText(qrCodes.valueAt(0).displayValue);
+                        }
+                    });
+                }
+            }
+        });
     }
 
+    public void launch_Login_Activity(){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+        finish();
+    }
 }
