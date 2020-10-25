9d5ae0f5e9a3f703073e8259726afc92d679d870
momo007Dev <m.valentin@students.ephec.be>
2020-10-25 19:31:35 +0100

    US => User can select all list in a dropdown menu done

--- makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/Page2.java
+++ makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/Page2.java
@@ -2,42 +2,122 @@ package com.morgan.make_kots_great_again;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.Html;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.Collections;
 
+import okhttp3.Call;
+import okhttp3.Callback;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Iterator;
+
 public class Page2 extends AppCompatActivity {
+
+    private String current_user_name;
+    private String current_user_token;
+    Spinner spinner;
+    ArrayAdapter<String> spinnerArrayAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.page2);
 
-        String lists[] = {"Ma liste Perso","Kot linux"};
+        SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0);
+        current_user_name = pref.getString("username", null);
+        current_user_token = pref.getString("token", null);
+
+        final ArrayList<String> lists = new ArrayList<>();
+
+
         String items[] = {"Banane", "Nutella", "Noisettes", "M&Ms", "Doritos", "Chocolat", "Glaces choco", "Frites"};
         ArrayList<String> quantity = new ArrayList<>();
         String items_quantity[] = {"1", "3", "5", "7", "2", "3", "1", "2"};
         Collections.addAll(quantity, items_quantity);
 
-        Spinner spinner = findViewById(R.id.dropdown_list);
+        spinner = findViewById(R.id.dropdown_list);
         ListView listView = findViewById(R.id.listview);
+        Button new_product = findViewById(R.id.button_new_product);
+        TextView welcome_user = findViewById(R.id.label_welcome);
 
-        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_dropdown_item, lists);
-        spinner.setAdapter(spinnerArrayAdapter);
+        welcome_user.setText(Html.fromHtml("Welcome back <span style=\"color:blue\">" + current_user_name + "</span> !"));
 
-        //generate list
+        try {
+            Get_Shopping_Lists_Request("http://172.18.0.3:8000/server/api/shoppingList/", lists);
+        } catch (Exception ignored) { }
+
+
+        new_product.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //TODO
+            }
+        });
+
+
         ArrayList<String> list = new ArrayList<String>();
         Collections.addAll(list, items);
 
-        //instantiate custom adapter
         MyCustomAdapter adapter = new MyCustomAdapter(list, quantity, this);
-
-        //handle listview and assign adapter
         listView.setAdapter(adapter);
+    }
+
+    public void Get_Shopping_Lists_Request(String url, final ArrayList<String> arrayList)  throws Exception {
+        OkHttpClient client = new OkHttpClient();
+
+        final Request request = new Request.Builder().header("Authorization", current_user_token).url(url).build();
+
+        client.newCall(request).enqueue(new Callback()
+        {
+            @Override
+            public void onFailure(Call call, IOException e) //Ok
+            {
+                Log.d("GET", "Error");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException
+            {
+                String responseBody = response.body().string();
+                Log.d("Get-Ok",responseBody);
+                try {
+                    JSONObject Jobject = new JSONObject(responseBody);
+                    JSONObject Jobject2 = Jobject.getJSONObject("shoppingList");
+
+                    Iterator<String> iter = Jobject2.keys(); //This should be the iterator you want.
+                    while(iter.hasNext()){
+                        String key = iter.next();
+                        arrayList.add(key);
+                        spinnerArrayAdapter = new ArrayAdapter<>(getBaseContext(), android.R.layout.simple_spinner_dropdown_item, arrayList);
+                        spinner.setAdapter(spinnerArrayAdapter);
+                        Log.d("ITER",key);
+                    }
+                } catch (JSONException ignored) { }
+
+                if(!responseBody.equals("Unauthorized")) {
+                    //OK
+                }
+                else {
+                    //ERROR
+                }
+            }
+        });
     }
 }
