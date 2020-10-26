6b0cdead8a415bf278e7c85e784f4b39e3fe552b
momo007Dev <m.valentin@students.ephec.be>
2020-10-26 13:52:49 +0100

    US-13: Consultation des elements d'une liste de course - mobile - DONE

--- makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/Page2.java
+++ makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/Page2.java
@@ -1,23 +1,26 @@
 package com.morgan.make_kots_great_again;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Html;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collections;
+import java.util.Iterator;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.Call;
 import okhttp3.Callback;
@@ -25,15 +28,18 @@ import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
 
-import java.io.IOException;
-import java.util.Iterator;
-
-public class Page2 extends AppCompatActivity {
+public class Page2 extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private String current_user_name;
     private String current_user_token;
-    Spinner spinner;
-    ArrayAdapter<String> spinnerArrayAdapter;
+    private Spinner spinner;
+    private ListView listView;
+    private ArrayAdapter<String> spinnerArrayAdapter;
+    private String current_list_selected;
+
+    final ArrayList<String> items = new ArrayList<>();
+    final ArrayList<String> items_quantity = new ArrayList<>();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -47,39 +53,19 @@ public class Page2 extends AppCompatActivity {
         final ArrayList<String> lists = new ArrayList<>();
 
 
-        String items[] = {"Banane", "Nutella", "Noisettes", "M&Ms", "Doritos", "Chocolat", "Glaces choco", "Frites"};
-        ArrayList<String> quantity = new ArrayList<>();
-        String items_quantity[] = {"1", "3", "5", "7", "2", "3", "1", "2"};
-        Collections.addAll(quantity, items_quantity);
-
         spinner = findViewById(R.id.dropdown_list);
-        ListView listView = findViewById(R.id.listview);
-        Button new_product = findViewById(R.id.button_new_product);
+        spinner.setOnItemSelectedListener(this);
+        listView = findViewById(R.id.listview);
         TextView welcome_user = findViewById(R.id.label_welcome);
 
         welcome_user.setText(Html.fromHtml("Welcome back <span style=\"color:blue\">" + current_user_name + "</span> !"));
 
         try {
-            Get_Shopping_Lists_Request("http://172.18.0.3:8000/server/api/shoppingList/", lists);
+            Get_Shopping_Lists("http://172.18.0.3:8000/server/api/shoppingList/", lists);
         } catch (Exception ignored) { }
-
-
-        new_product.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {
-                //TODO
-            }
-        });
-
-
-        ArrayList<String> list = new ArrayList<String>();
-        Collections.addAll(list, items);
-
-        MyCustomAdapter adapter = new MyCustomAdapter(list, quantity, this);
-        listView.setAdapter(adapter);
     }
 
-    public void Get_Shopping_Lists_Request(String url, final ArrayList<String> arrayList)  throws Exception {
+    public void Get_Shopping_Lists(String url, final ArrayList<String> arrayList) {
         OkHttpClient client = new OkHttpClient();
 
         final Request request = new Request.Builder().header("Authorization", current_user_token).url(url).build();
@@ -101,23 +87,74 @@ public class Page2 extends AppCompatActivity {
                     JSONObject Jobject = new JSONObject(responseBody);
                     JSONObject Jobject2 = Jobject.getJSONObject("shoppingList");
 
-                    Iterator<String> iter = Jobject2.keys(); //This should be the iterator you want.
+                    Iterator<String> iter = Jobject2.keys();
                     while(iter.hasNext()){
                         String key = iter.next();
                         arrayList.add(key);
                         spinnerArrayAdapter = new ArrayAdapter<>(getBaseContext(), android.R.layout.simple_spinner_dropdown_item, arrayList);
                         spinner.setAdapter(spinnerArrayAdapter);
-                        Log.d("ITER",key);
                     }
                 } catch (JSONException ignored) { }
-
-                if(!responseBody.equals("Unauthorized")) {
-                    //OK
-                }
-                else {
-                    //ERROR
-                }
             }
         });
+    }
+
+    // Dropdown menu with list "onChange functions"
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long l) {
+        //Lors d'un choix de liste, requete GET avec le nom de liste pour choper les elements de cette liste
+        current_list_selected = parent.getItemAtPosition(position).toString();
+        Log.d("SPINNER",current_list_selected);
+
+        Get_Shopping_Lists_items("http://172.18.0.3:8000/server/api/shoppingList/", items, items_quantity);
+        try {
+            TimeUnit.MILLISECONDS.sleep(500);
+            set_listview();
+        } catch (InterruptedException ignored) { }
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+        //Do Nothing
+    }
+
+    public void Get_Shopping_Lists_items(String url, final ArrayList<String> items, final ArrayList<String> quantity) {
+        OkHttpClient client = new OkHttpClient();
+
+        final Request request = new Request.Builder().header("Authorization", current_user_token).url(url).build();
+
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                Log.d("GET", "Error");
+            }
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException
+            {
+                String responseBody = response.body().string();
+                Log.d("GET", "Ok");
+                try {
+                    JSONObject Jobject = new JSONObject(responseBody);
+                    JSONObject Jobject2 = Jobject.getJSONObject("shoppingList");
+                    JSONArray Jarray = Jobject2.getJSONArray(current_list_selected);
+
+                    items.clear();
+                    quantity.clear();
+
+                    for(int i = 0; i < Jarray.length(); i++) {
+                        JSONObject object = Jarray.getJSONObject(i);
+                        String product_name = object.getString("product_name");
+                        String product_quantity = object.getString("quantity");
+                        items.add(product_name);
+                        quantity.add(product_quantity);
+                    }
+
+                } catch (JSONException ignored) { }
+            }
+        });
+    }
+    private void set_listview(){
+        listView.setAdapter(new MyCustomAdapter(items, items_quantity, getBaseContext()));
     }
 }
