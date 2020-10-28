6b2dbe2f5c2e98eaf69810b8ff29a3555a1b91fb
AutomeEdwin <automeedwin@gmail.com>
2020-10-29 00:53:13 +0100

    Added first unit test

---
+++ makekotsgreatagain/app/src/androidTest/java/com/morgan/make_kots_great_again/Page_QrTest.java
@@ -0,0 +1,15 @@
+package com.morgan.make_kots_great_again;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Page_QrTest {
+
+    @Test
+    public void isTokenWrong() {
+        Page_Qr page_qr = new Page_Qr();
+
+        assertTrue(page_qr.isTokenWrong("Unauthorized"));
+    }
+}
