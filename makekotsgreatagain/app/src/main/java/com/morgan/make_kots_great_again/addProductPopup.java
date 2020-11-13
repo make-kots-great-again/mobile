570694755f3ad0b816f631c4ced4178fd9e339fd
AutomeEdwin <automeedwin@gmail.com>
2020-11-13 17:17:36 +0100

    code refactoring

--- makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/addProductPopup.java
+++ makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/addProductPopup.java
@@ -13,12 +13,8 @@ import android.widget.Button;
 import android.widget.NumberPicker;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,24 +22,16 @@ import org.json.JSONObject;
 
 import java.io.IOException;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
-import java.util.Set;
 
 import okhttp3.Call;
 import okhttp3.Callback;
-import okhttp3.FormBody;
 import okhttp3.MediaType;
-import okhttp3.MultipartBody;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.RequestBody;
 import okhttp3.Response;
-import okhttp3.ResponseBody;
-import okio.Buffer;
-import okio.BufferedSink;
 
 public class addProductPopup extends Dialog {
 
@@ -63,7 +51,8 @@ public class addProductPopup extends Dialog {
     private Map<String, Integer> codes = new HashMap<>();
 
     //CONSTRUCTOR
-    public addProductPopup(final Activity activity){
+    public addProductPopup(final Activity activity)
+    {
         super(activity, R.style.Theme_AppCompat_DayNight_Dialog);
         setContentView(R.layout.add_new_product_popup);
 
@@ -77,18 +66,22 @@ public class addProductPopup extends Dialog {
         this.own = "Propriétaire :";
         this.search_bar_hint = "Rechercher un produit";
 
-        //Setup search bar
+        //Setup search bar ----------------------------------------------------------------------
         adapter = new ArrayAdapter<>(getContext(), android.R.layout.select_dialog_item, products);
         search_bar = (AutoCompleteTextView)findViewById(R.id.product_search_bar);
         search_bar.setThreshold(3);
         search_bar.setAdapter(adapter);
-        search_bar.addTextChangedListener(new TextWatcher() {
+        search_bar.addTextChangedListener(new TextWatcher()
+        {
             @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {}
 
             @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                if(s.length() == search_bar.getThreshold()){
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                if(s.length() == search_bar.getThreshold())
+                {
                     getProductsFromPattern((s.toString()));
                     refillAdapter();
                 }
@@ -98,14 +91,17 @@ public class addProductPopup extends Dialog {
             public void afterTextChanged(Editable s) {}
         });
 
+        //Setup UI zone for quantity selection -------------------------------------------------
         this.quantity = findViewById(R.id.text_quantity);
         this.nb_picker = findViewById(R.id.quantity_nb);
         this.nb_picker.setMaxValue(20);
         this.nb_picker.setMinValue(1);
 
+        // Setup UI zone for owner selection ---------------------------------------------------
         this.owner = findViewById(R.id.text_owner);
         this.radioGroup = findViewById(R.id.radio_group);
-        if(current_list_selected.contains("personal list")){
+        if(current_list_selected.contains("personal list"))
+        {
             this.owner.setVisibility(View.INVISIBLE);
             this.radioGroup.setVisibility(View.INVISIBLE);
         }
@@ -113,70 +109,14 @@ public class addProductPopup extends Dialog {
         this.meRadio.setChecked(true);
         this.groupeRadio = findViewById(R.id.btn_group);
 
+        //Setup Add Button ---------------------------------------------------------------------
         this.add_btn = findViewById(R.id.add_btn);
         add_btn.setOnClickListener(new View.OnClickListener()
         {
             @Override
-            public void onClick(View v) {
-                String url = "http://kotsapp.herokuapp.com/server/api/shoppingList/addProduct/" + current_group_id;
-
-                JSONObject json = new JSONObject();
-                try {
-                    json.put("code", codes.get(search_bar.getText().toString()));
-                    json.put("quantity", nb_picker.getValue());
-
-                    if(groupeRadio.isChecked()){
-                        json.put("groupProduct", true);
-                     }
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-
-                RequestBody body = RequestBody.create(MediaType.parse("application/json"), String.valueOf(json));
-
-                OkHttpClient client = new OkHttpClient();
-
-                Request request = new Request.Builder().header("Authorization", current_user_token).url(url).post(body).build();
-
-                client.newCall(request).enqueue(new Callback() {
-                    @Override
-                    public void onFailure(Call call, IOException e) {}
-
-                    @Override
-                    public void onResponse(Call call, Response response) throws IOException{
-                        try {
-                            String responseBody = response.body().string();
-
-                            final JSONObject Jobject = new JSONObject(responseBody);
-
-                            Log.d("ok", responseBody.toString());
-
-                            Log.d("msg", Jobject.getString("message"));
-                            Log.d("error", Jobject.getString("error"));
-
-                            //activity.Get_Shopping_Lists_items("https://kotsapp.herokuapp.com/server/api/shoppingList/", );
-
-                            if(Jobject.getString("success").equals("true")){
-                                //return to page2 update list
-                            }
-
-                            activity.runOnUiThread(new Runnable() {
-                                public void run() {
-                                    try {
-                                        Toast.makeText(activity, Jobject.getString("message"), Toast.LENGTH_SHORT).show();
-                                    } catch (JSONException e) {
-                                        e.printStackTrace();
-                                    }
-                                }
-                            });
-
-                        }catch (Exception e){
-                            e.printStackTrace();
-                        }
-
-                    }
-                });
+            public void onClick(View v)
+            {
+                addProductToList(activity);
             }
         });
     }
@@ -184,7 +124,8 @@ public class addProductPopup extends Dialog {
     /*
     * Build the popup when called
     */
-    public void build(){
+    public void build()
+    {
         show();
         this.search_bar.setHint(search_bar_hint);
         this.quantity.setText(qty);
@@ -195,7 +136,8 @@ public class addProductPopup extends Dialog {
     * Clear and refill the adapter of the AutoCompleteTextView
     * Called after each API request to get a list of product from the AutoCompleteTextView's pattern
     * */
-    public void refillAdapter(){
+    public void refillAdapter()
+    {
         adapter.clear();
 
         for(int i=0; i<products.size(); i++){
@@ -208,7 +150,8 @@ public class addProductPopup extends Dialog {
     *
     * @param patter : current text written in the AutoCompleteTextView
     * */
-    public void getProductsFromPattern(String pattern){
+    public void getProductsFromPattern(String pattern)
+    {
         String url = "http://kotsapp.herokuapp.com/server/api/products/" + pattern;
 
         OkHttpClient client = new OkHttpClient();
@@ -245,4 +188,91 @@ public class addProductPopup extends Dialog {
         });
     }
 
+    /*
+    * Function called when the user press the "add" button
+    * Display the response message when we get it
+    */
+    public void addProductToList(final Activity activity)
+    {
+        JSONObject json = makeJson();
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .header("Authorization", current_user_token)
+                .url("http://kotsapp.herokuapp.com/server/api/shoppingList/addProduct/" + current_group_id)
+                .post(RequestBody.create(MediaType.parse("application/json"), String.valueOf(json)))
+                .build();
+
+        client.newCall(request).enqueue(new Callback()
+        {
+            @Override
+            public void onFailure(Call call, IOException e)
+            {/*Nothing*/}
+
+            @Override
+            public void onResponse(Call call, Response response) throws IOException
+            {
+                try
+                {
+                    String responseBody = response.body().string();
+
+                    final JSONObject Jobject = new JSONObject(responseBody);
+
+                    //activity.Get_Shopping_Lists_items("https://kotsapp.herokuapp.com/server/api/shoppingList/", );
+
+                    activity.runOnUiThread(new Runnable()
+                    {
+                        @Override
+                        public void run()
+                        {
+                            try
+                            {
+                                Toast.makeText(activity, Jobject.getString("message"), Toast.LENGTH_SHORT).show();
+                            }
+                            catch(Exception e)
+                            {
+                                e.printStackTrace();
+                            }
+                        }
+                    });
+
+                    if(Jobject.getString("success").equals("true")){
+                        //return to page2 update list
+                    }
+
+                }
+                catch (Exception e)
+                {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /*
+    * Setup the json object to be put in the body of the API Request in order to add a product to the list
+    */
+    public JSONObject makeJson()
+    {
+        JSONObject json = new JSONObject();
+
+        try
+        {
+            json.put("code", codes.get(search_bar.getText().toString()));
+            json.put("quantity", nb_picker.getValue());
+
+            if(groupeRadio.isChecked())
+            {
+                json.put("groupProduct", true);
+            }
+
+        }
+        catch (JSONException e)
+        {
+            e.printStackTrace();
+        }
+
+        return json;
+    }
 }
