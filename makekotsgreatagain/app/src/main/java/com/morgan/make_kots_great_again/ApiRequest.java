ea6a910b995f343bdd5f5df61e7abf61f55aa1da
momo007Dev <m.valentin@students.ephec.be>
2020-11-27 21:57:09 +0100

    refactor of Login.java finished !

--- makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/ApiRequest.java
+++ makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/ApiRequest.java
@@ -7,7 +7,13 @@ import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -26,14 +32,68 @@ import okhttp3.Response;
 
 public class ApiRequest {
 
-    String token;
-    String user;
+    String token = new String();
+    String user = new String();
     String shopping_list_url = "https://kotsapp.herokuapp.com/server/api/shoppingList/";
+    String loggin_url = "https://kotsapp.herokuapp.com/server/api/login";
+    Activity activity;
 
+    /**
+     //--------------
+     // CONSTRUCTOR
+     //--------------
+     * @param activity
+     */
     public ApiRequest(Activity activity){
+        this.activity = activity;
         SharedPreferences pref = activity.getSharedPreferences("MyPref", 0);
         user = pref.getString("username", null);
         token = pref.getString("token", null);
+    }
+
+    /**
+     //---------------------------------------------------------
+     // Function that takes care of sending POST request to api
+     //---------------------------------------------------------
+     * @param username
+     * @param password
+     */
+    protected void login_post_request(TextView username, TextView password) {
+
+        JSONObject object = new JSONObject();
+        try {
+            object.put("pseudo",username.getText());
+            object.put("password",password.getText());
+        } catch (JSONException ignored) {}
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(com.android.volley.Request.Method.POST, loggin_url, object,
+                new com.android.volley.Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            user = response.getJSONObject("user").getString("username");
+                            token = response.getJSONObject("user").getString("token");
+
+                            SharedPreferences pref = activity.getSharedPreferences("MyPref", 0);
+                            SharedPreferences.Editor editor = pref.edit();
+                            editor.putString("username", user);
+                            editor.putString("token", token);
+                            editor.commit();
+
+                        } catch (JSONException ignored) { }
+                        Login login = new Login();
+                        login.launch_page2(activity);
+                    }
+                }, new com.android.volley.Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(activity, "⚠️ Incorrect username or password.", Toast.LENGTH_SHORT).show();
+                username.setText("");
+                password.setText("");
+            }
+        });
+        RequestQueue requestQueue = Volley.newRequestQueue(activity);
+        requestQueue.add(jsonObjectRequest);
     }
 
     /**
@@ -143,6 +203,15 @@ public class ApiRequest {
         } catch (InterruptedException ignored) { }
     }
 
+    /**
+     * ---------------------------------------------------------------------------------------
+     *  Method that gets all items of the selected shopping list via an api request
+     *  and launch Page3.java by building the listview with special class "MyCustomAdapter2"
+     * ---------------------------------------------------------------------------------------
+     * @param products
+     * @param selected_list
+     * @param activity
+     */
     protected void Get_items_page3(final ArrayList<Product> products, final String selected_list, final Activity activity) {
 
         OkHttpClient client = new OkHttpClient();
@@ -172,7 +241,7 @@ public class ApiRequest {
                         String product_name = object.getString("product_name");
                         String product_owner = object.getString("username");
                         String product_brand = object.getString("product_brand");
-                        if (product_owner.equals(user)){ product_owner = "Me"; } // US M12
+                        if (product_owner.equals(user)){ product_owner = "Me"; }
                         if (product_owner.equals("group")){ product_owner.toUpperCase(); }
                         String product_quantity = object.getString("quantity");
                         String product_uid = object.getString("shoppingListId");
@@ -184,13 +253,6 @@ public class ApiRequest {
                         else {
                             products.add(new Product(product_name, product_brand, product_owner, product_quantity, product_uid));
                         }
-
-                        /*
-                        // Permet de stocker l'ID du groupe dans une "shared preference"
-                        SharedPreferences pref = activity.getSharedPreferences("MyPref", 0);
-                        SharedPreferences.Editor editor = pref.edit();
-                        editor.putString("group_id", group_id);
-                        editor.commit();*/
                     }
 
                 } catch (JSONException ignored) { }
--- makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/Login.java
+++ makekotsgreatagain/app/src/main/java/com/morgan/make_kots_great_again/Login.java
@@ -6,11 +6,14 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Build;
 import android.os.Bundle;
 
@@ -33,33 +36,7 @@ import org.json.JSONObject;
 
 public class Login extends AppCompatActivity {
 
-    TextView username, password;
-
-    // Data needed to connect to API (ip, port and connection type)
-    private final String ip = "172.18.0.3";
-    private final int port = 8000;
-    private final boolean connection_type_local = false; // true = Docker local server, false = website
-    private final NetworkChecks checkClass = new NetworkChecks();
-
-    //Info about user like username, and token to simplify GET request
-    private String current_user_name = "";
-    private String current_user_token = "";
-
     private static final int MY_CAMERA_REQUEST_CODE = 100;
-
-    // Getters for username and token
-    public String getCurrent_user_name() { return current_user_name; }
-
-    public String getCurrent_user_token() { return current_user_token; }
-
-    // Getters for ip and port
-    public String getIp() {
-        return ip;
-    }
-
-    public String getPort() {
-        return Integer.toString(port);
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -68,31 +45,16 @@ public class Login extends AppCompatActivity {
 
         final Button button_sign = findViewById(R.id.button_sign);
         final Button button_scan = findViewById(R.id.button_scan);
-        username = findViewById(R.id.edit_mail);
-        password = findViewById(R.id.edit_password);
-
-        final Context context = this;
+        final TextView username = findViewById(R.id.edit_mail);
+        final TextView password = findViewById(R.id.edit_password);
 
         button_sign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                ApiRequest apiRequest = new ApiRequest(Login.this);
                 // Checks if the user is connected to internet
-                if (!checkClass.isConnectedToInternet(context)) {
-                    Toast.makeText(getBaseContext(), "⚠️ No internet connection !", Toast.LENGTH_SHORT).show();
-                }
-
-                if (connection_type_local){
-                    // If connection to host cannot be established
-                    if (!checkClass.isHostUp(ip)){
-                        Toast.makeText(getBaseContext(), "⚠️ Error connecting to Database", Toast.LENGTH_SHORT).show();
-                    }
-                    // If everything is ok, connects to API and sends data
-                    else {
-                        login_post_request();
-                    }
-                }
-                else{
-                    login_post_request();
+                if (isConnectedToInternet()) {
+                    apiRequest.login_post_request(username, password);
                 }
             }
         });
@@ -101,32 +63,25 @@ public class Login extends AppCompatActivity {
             @RequiresApi(api = Build.VERSION_CODES.M)
             @Override
             public void onClick(View view) {
-                if (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
-                {
+                if (ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
                     launch_QR_scan_activity();
                 }
-                else
-                {
+                else {
                     requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
                 }
             }
         });
     }
 
-
     @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults)
-    {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        if (requestCode == MY_CAMERA_REQUEST_CODE)
-        {
-            if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
-            {
+        if (requestCode == MY_CAMERA_REQUEST_CODE) {
+            if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 Toast.makeText(this, "camera permission granted", Toast.LENGTH_LONG).show();
                 launch_QR_scan_activity();
             }
-            else
-            {
+            else {
                 Toast.makeText(this, "camera permission not granted", Toast.LENGTH_LONG).show();
             }
         }
@@ -137,10 +92,27 @@ public class Login extends AppCompatActivity {
      // Function that destroy current activity and launch "Page2"
      //-----------------------------------------------------------
      */
-    private void launch_page2(){
-        Intent intent = new Intent(this, Page2.class);
-        startActivity(intent);
-        finish();// Kills curent activity
+    protected void launch_page2(Activity activity){
+        Intent intent = new Intent(activity, Page2.class);
+        activity.startActivity(intent);
+        activity.finish();// Kills curent activity
+    }
+
+    /**
+     //-----------------------------------------------------------
+     // Function that checks if user is connected to internet
+     //-----------------------------------------------------------
+     * @return
+     */
+    private boolean isConnectedToInternet() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        boolean result = networkInfo != null && networkInfo.isConnected();
+        if (!result){
+            Toast.makeText(this, "⚠️ No internet connection !", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 
     /**
@@ -151,48 +123,5 @@ public class Login extends AppCompatActivity {
     private void launch_QR_scan_activity(){
         Intent intent = new Intent(this, Page_Qr.class);
         startActivity(intent);
-    }
-
-    /**
-     //---------------------------------------------------------
-     // Function that takes care of sending POST request to api
-     //---------------------------------------------------------
-     */
-    private void login_post_request() {
-
-        String url = checkClass.connection_method(connection_type_local);
-        JSONObject object = new JSONObject();
-        try {
-            object.put("pseudo",username.getText());
-            object.put("password",password.getText());
-        } catch (JSONException ignored) {}
-
-        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, url, object,
-                new Response.Listener<JSONObject>() {
-                    @Override
-                    public void onResponse(JSONObject response) {
-                        try {
-                            current_user_name = response.getJSONObject("user").getString("username");
-                            current_user_token = response.getJSONObject("user").getString("token");
-
-                            SharedPreferences pref = getApplicationContext().getSharedPreferences("MyPref", 0); // 0 - for private mode
-                            SharedPreferences.Editor editor = pref.edit();
-                            editor.putString("username", current_user_name);
-                            editor.putString("token", current_user_token);
-                            editor.commit();
-
-                        } catch (JSONException ignored) { }
-                        launch_page2();
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast.makeText(getBaseContext(), "⚠️ Incorrect username or password.", Toast.LENGTH_SHORT).show();
-                username.setText("");
-                password.setText("");
-            }
-        });
-        RequestQueue requestQueue = Volley.newRequestQueue(getApplicationContext());
-        requestQueue.add(jsonObjectRequest);
     }
 }
